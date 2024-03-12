10c53290ca888f681b4646608382c1afdc8ebf77
TaeJuneJoung <jtj0525@gmail.com>
2024-03-12 13:40:23 +0900

    Library styled-components

--- 06-styling/src/components/Header.jsx
+++ 06-styling/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+// import classes from "./Header.module.css";
+import StyledHeader from "./StyledHeader";
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
-    </header>
+    </StyledHeader>
   );
 }
