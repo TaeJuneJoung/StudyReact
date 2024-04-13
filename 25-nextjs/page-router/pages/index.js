b6db51aa49e20908c22ee225d1024c88f38747f0
TaeJuneJoung <jtj0525@gmail.com>
2024-04-13 23:46:54 +0900

    기본 형식 구성

--- 25-nextjs/page-router/pages/index.js
+++ 25-nextjs/page-router/pages/index.js
@@ -1,5 +1,33 @@
+import MeetupList from "@/components/meetups/MeetupList";
+import { useEffect, useState } from "react";
+
+const DUMMY_LIST = [
+  {
+    id: "m1",
+    title: "눈물의 여왕",
+    image:
+      "https://i.namu.wiki/i/1FQZf-g3pMrCoNp9_MOnQmAqxoFCId259JV5hotzrkoLDYmhdmnlMytnKdstBtMsrEyjOCioYAbIwjfVlq51Iw.webp",
+    address: "Korea",
+    description: "눈물의 여왕 tvn 9:20 p.m. 시작",
+  },
+  {
+    id: "m2",
+    title: "눈물의 여왕",
+    image:
+      "https://i.namu.wiki/i/1FQZf-g3pMrCoNp9_MOnQmAqxoFCId259JV5hotzrkoLDYmhdmnlMytnKdstBtMsrEyjOCioYAbIwjfVlq51Iw.webp",
+    address: "Korea",
+    description: "눈물의 여왕 tvn 9:20 p.m. 시작",
+  },
+];
+
 function HomePage() {
-  return <h1>The Home Page</h1>;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setLoadedMeetups(DUMMY_LIST);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
---
+++ 25-nextjs/page-router/pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+function newMeetupPage() {
+  function addMeetupHandler(enteredMeetupData) {}
+  return <NewMeetupForm onAddMeetup={() => addMeetupHandler()} />;
+}
+
+export default newMeetupPage;
