76b4a2137f92495b0f86793d38fd4e6dc53f5336
TaeJuneJoung <jtj0525@gmail.com>
2024-05-06 00:43:52 +0900

    :bug:버그 및 데이터 타입 설정

--- js_to_ts/10-advanced-state-management-with-context-usereducer/src/data/dummy-products.ts
+++ js_to_ts/10-advanced-state-management-with-context-usereducer/src/data/dummy-products.ts
@@ -1,9 +1,9 @@
-import mochaOvercoat from './assets/mocha-overcoat.jpg'
-import dreamGown from './assets/dream-gown.jpg'
-import rainJacket from './assets/rain-jacket.jpg'
-import merlotSuit from './assets/merlot-suit.jpg'
-import moonlightDress from './assets/moonlight-dress.jpg'
-import denimPioneer from './assets/denim-pioneer.jpg'
+import mochaOvercoat from '../assets/mocha-overcoat.jpg'
+import dreamGown from '../assets/dream-gown.jpg'
+import rainJacket from '..//assets/rain-jacket.jpg'
+import merlotSuit from '../assets/merlot-suit.jpg'
+import moonlightDress from '../assets/moonlight-dress.jpg'
+import denimPioneer from '../assets/denim-pioneer.jpg'
 
 export type PRODUCT_TYPE = {
   id: string
